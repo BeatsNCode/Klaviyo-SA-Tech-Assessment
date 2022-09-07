991ee750498f8d9cb777d17c10c2528246d96e66
Jean Augustin <jre.augustin@gmail.com>
2022-09-07 18:54:17 -0400

    Successfully pull List Data from Klaviyo List V2 API

--- app.py
+++ app.py
@@ -14,4 +14,14 @@ kickbox_api_key = os.getenv('KICKBOX_API_KEY')
 twilio_api_key = os.getenv('TWILIO_API_KEY')
 
 
+# First, the Klaviyo List V2 API API will be leveraged to pull profile data, specifically `Email` and `Phone Number`
+klaviyo_list_id = 'UwaMMy'
 
+klaviyo_list_url = f'https://a.klaviyo.com/api/v2/group/{klaviyo_list_id}/members/all?api_key={klaviyo_private_api_key}'
+
+headers = {"Accept": "application/json"}
+
+response = requests.get(klaviyo_list_url, headers=headers)
+
+list_data = response.text
+print(list_data)
